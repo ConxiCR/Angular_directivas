91355a0f1c261eadb1fe8f9bbdda4fc285a18f57
ConxiCr <conxicr@hotmail.com>
2021-03-16 09:15:27 +0100

    directiva de atributo ngStyle
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DIRECTIVAS';
+
+  propiedadesParrafo: any;
+
+  constructor(){
+    this.propiedadesParrafo = {
+      color: 'red',
+       fontSize:'24px'
+    }
+  }
+
+  cambiaColor(pColor:string){
+    switch(pColor){
+      case 'a':
+        this.propiedadesParrafo.color = 'yellow';
+        break;
+      case 'r':
+        this.propiedadesParrafo.color = 'red';
+        break;
+      case 'v':
+        this.propiedadesParrafo.color = 'green';
+        break;
+    }
+
+  }
+
+  
 }
