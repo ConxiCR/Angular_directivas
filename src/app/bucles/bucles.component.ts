caa6c4f5aa3bafc4c6d7804fb6f1e29ddca38a54
ConxiCr <conxicr@hotmail.com>
2021-03-22 12:08:00 +0100

    ngFor (mejorando diseño)
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- src/app/bucles/bucles.component.ts
+++ src/app/bucles/bucles.component.ts
@@ -9,16 +9,28 @@ export class BuclesComponent implements OnInit {
   personas:any[];
 
   constructor() { 
-    this.personas = [
+    /*this.personas = [
       {nombre:'Mario', apellidos:'Girón', edad: 36},
       {nombre:'Rosa', apellidos:'Vazquez', edad: 50},
       {nombre:'Maria', apellidos:'Fernández', edad: 26},
       {nombre:'Conxi', apellidos:'Rodríguez', edad: 45},
       {nombre:'Samuel', apellidos:'Tomás', edad: 16}
+    ]*/
+    this.personas = [
+      {nombre:'Mario', apellidos:'Girón Pabón', calle: 'ctra. de Siles, 16', telefono:'741 385 960'},
+      {nombre:'Rosa', apellidos:'Vazquez Carmona', calle: 'Avda.Rio Nalon, 7', telefono:'741 385 960'},
+      {nombre:'Maria', apellidos:'Fernández Sierra', calle: 'Plazuela do Porto, 90', telefono:'741 385 960'},
+      {nombre:'Conxi', apellidos:'Rodríguez Yañez', calle: 'Salzillo, 60', telefono:'741 385 960'},
+      {nombre:'Samuel', apellidos:'Tomás Godoy', calle: 'Cañadilla, 47', telefono:'741 385 960'}
     ]
   }
 
   ngOnInit(): void {
   }
-
+  onClick(){
+    //para agregar una nueva persona
+      this.personas.push({
+        nombre:'Mark', apellidos:'Eusebio', calle:'Gran Via, 21', telefono: '3343 9989'
+      })
+  }
 }
