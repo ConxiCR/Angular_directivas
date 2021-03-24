fbfa464bf1d94d6bb57a1a4ee045649138c01cbf
ConxiCr <conxicr@hotmail.com>
2021-03-24 08:24:47 +0100

    @HostListener
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- src/app/directivas/subrayado.directive.ts
+++ src/app/directivas/subrayado.directive.ts
@@ -1,19 +1,26 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[subrayado]'
 })
 export class SubrayadoDirective {
 
-  constructor(elem: ElementRef, renderer: Renderer2) {
+  constructor(private elem: ElementRef, private renderer: Renderer2) {
     //console.log('Se está usando la directiva');
     //console.log(elem);
     //console.log(elem.nativeElement);
     //cambiar al elemento sobre el que aplico la directiva el estilo textdecoration
     //elem.nativeElement.style.textDecoration = 'underline';
     //para modificar el estilo independientemente de la plataforma
-    renderer.setStyle(elem.nativeElement, 'textDecoration', 'underline');
-    renderer.setStyle(elem.nativeElement, 'color', 'indigo');
+    this.renderer.setStyle(this.elem.nativeElement, 'textDecoration', 'underline');
+    this.renderer.setStyle(this.elem.nativeElement, 'color', 'indigo');
    }
-
+   //cuando el mouse pase por encima, cambiará el color
+   @HostListener('mouseover') onHover(){
+    this.renderer.setStyle(this.elem.nativeElement, 'color', 'green');
+   }
+   //para revertir el color
+   @HostListener('mouseout') ononMouseOut(){
+    this.renderer.setStyle(this.elem.nativeElement, 'color', 'pink');
+   }
 }
