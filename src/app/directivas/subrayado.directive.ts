fc644d9bb815aac8df7dda8fc95e6afb3befc9ad
ConxiCr <conxicr@hotmail.com>
2021-03-24 06:33:52 +0100

    renderer (plataformas)
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- src/app/directivas/subrayado.directive.ts
+++ src/app/directivas/subrayado.directive.ts
@@ -1,16 +1,19 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[subrayado]'
 })
 export class SubrayadoDirective {
 
-  constructor(elem: ElementRef) {
+  constructor(elem: ElementRef, renderer: Renderer2) {
     //console.log('Se está usando la directiva');
     //console.log(elem);
     //console.log(elem.nativeElement);
     //cambiar al elemento sobre el que aplico la directiva el estilo textdecoration
-    elem.nativeElement.style.textDecoration = 'underline';
+    //elem.nativeElement.style.textDecoration = 'underline';
+    //para modificar el estilo independientemente de la plataforma
+    renderer.setStyle(elem.nativeElement, 'textDecoration', 'underline');
+    renderer.setStyle(elem.nativeElement, 'color', 'indigo');
    }
 
 }
